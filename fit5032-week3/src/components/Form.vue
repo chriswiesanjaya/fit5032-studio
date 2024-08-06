47ccbb2b7a109e869b1ce90563cb301a7e1e3c55
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-08-07 00:51:45 +1000

    week3 - activity 6 (clear button not working)

--- fit5032-week3/src/components/Form.vue
+++ fit5032-week3/src/components/Form.vue
@@ -80,11 +80,65 @@
             <button type="submit" class="btn btn-secondary" @click="clearForm">Clear</button>
           </div>
         </form>
+        <div class="row mt-5" v-if="submittedCards.length">
+          <div class="d-flex flex-wrap justify-content-start">
+            <div
+              v-for="(card, index) in submittedCards"
+              :key="index"
+              class="card m-2"
+              style="width: 18rem"
+            >
+              <div class="card-header">User Information</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Username: {{ card.username }}</li>
+                <li class="list-group-item">Password: {{ card.password }}</li>
+                <li class="list-group-item">
+                  Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
+                </li>
+                <li class="list-group-item">Gender: {{ card.gender }}</li>
+                <li class="list-group-item">Reason: {{ card.reason }}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<style scoped></style>
+const formData = ref({
+  username: '',
+  password: '',
+  isAustralian: false,
+  reason: '',
+  gender: ''
+})
+
+const submittedCards = ref([])
+
+const submitForm = () => {
+  submittedCards.value.push({
+    ...formData.value
+  })
+}
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.card-header {
+  background-color: #275fda;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+.list-group-item {
+  padding: 10px;
+}
+</style>
