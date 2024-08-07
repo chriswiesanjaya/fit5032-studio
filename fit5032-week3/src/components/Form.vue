97dbe17936cc6eca23b21ee838cbaf009261ba2f
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-08-07 13:38:36 +1000

    week 3 - act7 completed

--- fit5032-week3/src/components/Form.vue
+++ fit5032-week3/src/components/Form.vue
@@ -31,11 +31,11 @@
         <h1 class="text-center">User Information Form</h1>
         <form @submit.prevent="submitForm">
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-md-6 col-sm-6 col-6">
               <label for="username" class="form-label">Username</label>
               <input type="text" class="form-control" id="username" v-model="formData.username" />
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 col-sm-6 col-6">
               <label for="password" class="form-label">Password</label>
               <input
                 type="password"
@@ -46,7 +46,7 @@
             </div>
           </div>
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-md-6 col-sm-6 col-6">
               <div class="form-check>">
                 <input
                   type="checkbox"
@@ -57,7 +57,7 @@
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 col-sm-6 col-6">
               <label for="gender" class="form-label">Gender</label>
               <select class="form-select" id="gender" v-model="formData.gender">
                 <option value="male">Male</option>
@@ -126,7 +126,13 @@ const submitForm = () => {
 }
 
 const clearForm = () => {
-  submittedCards.value = []
+  formData.value = {
+    username: '',
+    password: '',
+    isAustralian: false,
+    reason: '',
+    gender: ''
+  }
 }
 </script>
 
