4509f021fab314f1c6f14b916c89fd68e0fcce73
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-08-27 12:54:01 +1000

    week 5 - act4 clone completed

--- fit5032-week5-clone/src/components/LibraryRegistrationForm.vue
+++ fit5032-week5-clone/src/components/LibraryRegistrationForm.vue
@@ -8,7 +8,8 @@ const formData = ref({
   password: '',
   isAustralian: false,
   reason: '',
-  gender: ''
+  gender: '',
+  suburb: 'Clayton'
 })
 
 const submittedCards = ref([])
@@ -170,6 +171,10 @@ const validateReason = (blur) => {
               v-model="formData.reason"
             ></textarea>
             <div v-if="errors.friendMessage" class="text-success">{{ errors.friendMessage }}</div>
+          </div>
+          <div class="mb-3">
+            <label for="reason" class="form-label">Suburb</label>
+            <input type="text" class="form-control" id="suburb" v-bind:value="formData.suburb" />
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
