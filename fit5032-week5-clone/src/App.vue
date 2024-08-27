b95a8fa27ba67ec57eac5baff7c52242f08a67fa
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-08-27 13:09:34 +1000

    week 5 - act 5 clone completed

--- fit5032-week5-clone/src/App.vue
+++ fit5032-week5-clone/src/App.vue
@@ -10,8 +10,9 @@ import LibraryRegistrationForm from './views/HomeView.vue'
   </header>
 
   <main>
-    <LibraryRegistrationForm />
+    <!-- <LibraryRegistrationForm /> -->
     <!-- <JSONLab /> -->
+    <router-view></router-view>
   </main>
 </template>
 
--- fit5032-week5-clone/src/views/AboutView.vue
+++ fit5032-week5-clone/src/views/AboutView.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="about">
-    <h1>About Our Library</h1>
-    <p>
-      Welcome to our digital library! We're dedicated to providing a vast collection of books and
-      resources to our community.
-    </p>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">About Our Library</h1>
+        <p>
+          Welcome to our digital library! We're dedicated to providing a vast collection of books
+          and resources to our community.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,4 +16,19 @@
 // No script needed for now
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  max-width: 80vw;
+  margin: 0 auto;
+  padding: 20px;
+  /* background-color: #e0bfbf; */
+  border-radius: 10px;
+}
+</style>
+
+<!-- <h1>About Our Library</h1>
+    <p>
+      Welcome to our digital library! We're dedicated to providing a vast collection of books and
+      resources to our community.
+    </p> -->
