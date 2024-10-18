a85a89cd7fb4cf95f265fdb55f60cba5319882c3
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-10-18 13:16:26 +1100

    change vue config js

--- fit5032-week11/vue.config.js
+++ fit5032-week11/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/fit5032-studio' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/fit5032-studio/fit5032-week11' : '/'
 }
