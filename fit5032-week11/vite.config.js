a3b1bcea77e5509101e8cf779c2a3cd2392c67cc
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-10-18 14:41:30 +1100

    test

--- fit5032-week11/vite.config.js
+++ fit5032-week11/vite.config.js
@@ -6,7 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: process.env.CF_PAGES
+    ? '/' // For Cloudflare Pages
+    : process.env.GITHUB_ACTIONS
+      ? '/fit-5032/' // For GitHub Pages
+      : '/', // For local debugging
   build: {
     outDir: 'dist'
   },
