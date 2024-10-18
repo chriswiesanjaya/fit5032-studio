896bf937895a525ae94b233cdc95f7ac810578d8
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-10-18 13:25:56 +1100

    test

--- fit5032-week11/vue.config.js
+++ fit5032-week11/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/fit5032-studio/fit5032-week11' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/fit5032-studio/fit5032-week11/' : '/'
 }
